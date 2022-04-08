9f54816b943feb821fc13228b16ca44044085d39
LőrinczAlexandra <lorincz.alexandra@edu.bme.hu>
2022-04-08 12:07:00 +0200

    ochestrator and prometheus component added

---
+++ cypress-tutorial-build-todo-starter/flask/orc/app.py
@@ -0,0 +1,35 @@
+import os
+import docker
+from flask import Flask
+import xml.etree.ElementTree as ET
+app = Flask(__name__)
+
+@app.route("/run-cypress")
+def run_cypress():
+    client = docker.from_env()
+    client.containers.run("cypress-cypress-1")
+
+@app.route("/")
+def home():
+    client = docker.from_env()
+    print(client.containers.list())
+    return "HELLO!"
+
+@app.route("/results")
+def res():
+    longlist = list()
+    # what is the path??
+    for file in os.listdir('/code/cypress-tutorial-build-todo-starter/results'):
+        tree = ET.parse(file)
+        root = tree.getroot()
+        for child in root:
+            c = str(child.tag) + str(child.attrib)
+            longlist.append(c)
+            for grandchild in child:
+                c = str(grandchild.tag) + str(grandchild.attrib)
+                longlist.append(c)
+    return longlist
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ cypress-tutorial-build-todo-starter/flask/run.py
@@ -0,0 +1,4 @@
+from orc import app
+import os
+if __name__ == '__main__':
+    app.app.run(host='0.0.0.0', port=5000)
